f395810a90a35799e3ed3419d174ff04df51fe48
jonatha tridente <jonathapietro@hotmail.com>
2022-05-22 21:28:04 -0300

    acesso ao banco

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Npgsql;
 namespace chat_backend.Controllers;
 
 [ApiController]
@@ -10,6 +10,13 @@ public class LoginController: ControllerBase {
     [HttpPost(Name = "Logar")]
 
     public bool Post(LoginDTO login) {
-        return true;
+        var conexao = "Host=localhost;Username=postgres;Password=102030;Database=DBChat_Test";
+        var con = new NpgsqlConnection (conexao);
+        con.Open();
+        var comando = new NpgsqlCommand ($"Select 1 from public.login where username='{login.Login}' and senha='{login.Senha}'", con);
+        var resultado = int.TryParse(comando.ExecuteScalar()?.ToString(),out int Result);
+        con.Close ();
+
+        return resultado;
     }
 }
